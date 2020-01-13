403ad9184b661cbb3b96bf19ffd732caf2ef2798
xiaolei <tristones.liu@gmail.com>
2020-01-13 14:08:08 -0500

    params and body interpolation

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const specJson = protagonist.parseSync(specFile);
 const host = getHost(specJson.content);
 const specs = retrieve(specJson.content);
 
-// only generate http spects
+// only generate http specs
 if (!argv.d) {
   console.log(specs);
   return
@@ -44,18 +44,32 @@ async.eachSeries(reqData, (r, cb) => {
     return;
   }
 
-  //  interpolate path param
-  const path = _.reduce(r.params, (p, v, k) => p.replace(`{${k}}`, v),
-                        spec.url);
-  // TODO: interpolate query params
-  console.log(chalk.gray(`${spec.method} ${spec.url}`));
+  // interpolate path param
+  var path = _.reduce(r.params, (p, v, k) => p.replace(`{${k}}`, v),
+                      spec.url).replace(/{|}/g, '');
+  var query = '';
+  // interpolate query params
+  var paths = path.split('?');
+  if (paths.length > 1) {
+    path = paths[0];
+    query = _.chain(paths[1]).split(',').
+      map(q => [q, _.get(r.params, q)]).
+      filter(x => !!x[1]).
+      map(x => `${x[0]}=${encodeURIComponent(x[1])}`).
+      join('&').
+      value();
+  }
+
+  var url = `https://${host}${path}${query?'?'+query:''}`;
 
   request({
     method: spec.method,
-    url: `https://${host}${path}`,
-    headers: r.headers
-    // TODO: add body
+    url: url,
+    headers: r.headers,
+    body: r.body ? JSON.stringify(r.body): undefined
   }, (err, res, body) => {
+    console.log(chalk.gray(`${spec.method} ${url}`));
+    // check status code
     assert.equal(res.statusCode, 200, 'get 200 status code');
 
     if (spec.schema) {
